bbeefdc65cb6f11a75619735612ebadfe2a8464a
Himanshu000111 <145653053+Himanshu000111@users.noreply.github.com>
2024-06-24 08:36:46 +0000

    Weather App

--- src/components/HeaderBtns.jsx
+++ src/components/HeaderBtns.jsx
@@ -4,17 +4,35 @@ function HeaderBtns({setQuery}) {
 
 const cities = [
   {
-    name: ''
-  }
+    name: 'London',
+    id: 1
+  },
+  {
+    name: 'Sydney',
+    id: 2
+  },
+  {
+    name: 'Tokyo',
+    id: 3
+  },
+  {
+    name: 'Paris',
+    id: 4
+  },
+  {
+    name: 'Canada' ,
+    id: 5
+  },
 ]
 
   return (
     <div className='links flex gap-10 items-center justify-around my-6'>
-    {[{name: 'London', id: 1}, {name: 'Sydney', id: 2}, {name: 'Tokyo', id: 3}, { name: 'Paris', id: 4}, {name: 'Torronto', id:     
-     5}].map((city)=>(<button key={city.id} className = {`text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-l transition ease-in`} 
-       onClick={() => setQuery({q: city.name})}>
-      {city.name}
-      </button>))} 
+      {cities.map((city) => (
+        <button key={city.id} 
+        className = {`text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-l transition ease-in`}
+        onClick={() => setQuery({q: city.name})}>
+          {city.name}</button>
+      ))}
     </div>
   )
 }
